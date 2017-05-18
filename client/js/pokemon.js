967a3529392836d8258044157afcd6397accf84c
Tom <Tom Hewuse>
2017-05-18 13:58:44 -0500

    sockets added

--- client/js/pokemon.js
+++ client/js/pokemon.js
@@ -6,6 +6,10 @@
 
  $(document).ready(function() {
 
+     // this triggers the connection event in our server!
+     var socket = io.connect();
+     // we'll write all the socket stuff after the above line!
+
      getPics();
 
      $('img').click(function() {
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 var bp = require('body-parser');
-//var io = require('socket.io').listen(app);
 var path = require('path');
 var port = 8000;
-const pokenmon = require('pokemon');
-
+const pokemon = require('pokemon');
 
 app.use(express.static(path.join(__dirname + '/client/')));
 app.use(bp.urlencoded({ extended: true }));
@@ -13,12 +11,13 @@ app.set('views', path.join(__dirname + '/client/templates/'));
 app.set('view engine', 'ejs');
 require('./server/config/routes.js')(app);
 
-app.listen(port, function() {
-    console.log('server is on');
-    console.log(pokenmon.all());
-})
+var server = app.listen(port, function() {
+    console.log('server is listening on port 8000');
+});
 
-// io.sockets.on('connection', function(socket) {
-//     console.log("We are using socket!");
-//     console.log(socket.id);
-// })
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+    console.log("We are using socket!");
+    console.log(socket.id);
+});
